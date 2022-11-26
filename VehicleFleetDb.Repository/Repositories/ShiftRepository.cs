f6297aa41aa9f0d0efe26059f31dff4dc3a49e47
Bence Farkas <bence.farkas01@gmail.com>
2022-11-26 22:54:09 +0100

    Repo IsVirtual Update fix

--- VehicleFleetDb.Repository/Repositories/ShiftRepository.cs
+++ VehicleFleetDb.Repository/Repositories/ShiftRepository.cs
@@ -22,7 +22,10 @@ namespace VehicleFleetDb.Repository
             var old = Read(item.ShiftId);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
--- VehicleFleetDb.Repository/Repositories/VehicleRepository.cs
+++ VehicleFleetDb.Repository/Repositories/VehicleRepository.cs
@@ -22,7 +22,10 @@ namespace VehicleFleetDb.Repository
             var old = Read(item.Registration);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }

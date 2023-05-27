e298aab93b78da1662571cc95ed818aa7d062a1d
Bence Farkas <bence.farkas01@gmail.com>
2023-05-27 09:37:32 +0200

    Create Working

--- BCZZ3O_HFT_2022231.WPFClient/MainWindowViewModel.cs
+++ BCZZ3O_HFT_2022231.WPFClient/MainWindowViewModel.cs
@@ -52,6 +52,7 @@ namespace BCZZ3O_HFT_2022231.WPFClient
 
         public MainWindowViewModel()
         {
+            
             if (!IsInDesignMode)
             {
                 Vehicles = new RestCollection<Vehicle>("http://localhost:47322/", "vehicle");
@@ -60,8 +61,8 @@ namespace BCZZ3O_HFT_2022231.WPFClient
                 {
                     Vehicles.Add(new Vehicle()
                     {
-                        Registration = "AAA111",
-                        DisplayReg = "AAA111",
+                        Registration = SelectedVehicle.DisplayReg,
+                        DisplayReg = SelectedVehicle.DisplayReg,
                         Manufacturer = "aaa",
                         Model = "bbb",
                         Length = 12,
@@ -84,6 +85,7 @@ namespace BCZZ3O_HFT_2022231.WPFClient
                 }
                 );
             }
+            SelectedVehicle = new Vehicle();
         }
     }
 }
